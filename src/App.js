8bb61a6d852f5c8b6369925a7fa7b1da727e1747
rashmin66 <hansi20ra@gmail.com>
2022-11-25 01:47:10 +0530

    comments

---
+++ src/App.js
@@ -0,0 +1,34 @@
+function App() {
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+    return digits;
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <span>(0)</span> 0
+      </div>
+      <div className="operator">
+        <button>/</button>
+        <button>*</button>
+        <button>+</button>
+        <button>-</button>
+        <button>DEL</button>
+      </div>
+
+      <div className="digits">
+        {createDigits()}
+        <button>1</button>
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
